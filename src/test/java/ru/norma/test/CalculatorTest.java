24bf9f7e3f8996c644d01c601cc73ad0b8ef81f3
Курдюков М.А <Kurdyukov_MA@rnb.local>
2019-09-09 14:15:33 +0300

    change01

--- src/test/java/ru/norma/test/CalculatorTest.java
+++ src/test/java/ru/norma/test/CalculatorTest.java
@@ -2,55 +2,91 @@ package ru.norma.test;
 
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-public class CalculatorTest {
+class CalculatorTest {
 
     private Calculator calculator;
 
     @BeforeEach
-    public void setup() {calculator = new Calculator();}
+    void setup() {
+        calculator = new Calculator();
+    }
 
     @Test
-    public void addTwoPositiveResPos() {assertEquals(calculator.add(1,6), 7);}
+    @DisplayName("Верное сложение двух положительных")
+    void addTwoPositiveResPos() {
+        assertEquals(calculator.add(1, 6), 7);
+    }
 
     @Test
-    public void addTwoPositiveResNeg() {assertNotEquals(calculator.add(1,6), 5);}
+    @DisplayName("Неверное сложение двух положительных")
+    void addTwoPositiveResNeg() {
+        assertNotEquals(calculator.add(1, 6), 5);
+    }
 
     @Test
-    public void addTwoNegativeResPos() {assertEquals(calculator.add(-10, -5), -15);}
+    @DisplayName("Верное сложение двух отрицательных")
+    void addTwoNegativeResPos() {
+        assertEquals(calculator.add(-10, -5), -15);
+    }
 
     @Test
-    public void addTwoNegativeResNeg() {assertNotEquals(calculator.add(-10, -5), -5);}
+    @DisplayName("Неверное сложение двух отрицательных")
+    void addTwoNegativeResNeg() {
+        assertNotEquals(calculator.add(-10, -5), -5);
+    }
 
     @Test
-    public void addOppositeResPos() {assertEquals(calculator.add(-5, 11), 6);}
+    @DisplayName("Верное сложение чисел с разным знаком")
+    void addOppositeResPos() {
+        assertEquals(calculator.add(-5, 11), 6);
+    }
 
     @Test
-    public void addOppositeResNeg() {assertNotEquals(calculator.add(5, -11), 17);}
+    @DisplayName("Неверное сложение чисел с разным знаком")
+    void addOppositeResNeg() {
+        assertNotEquals(calculator.add(5, -11), 17);
+    }
 
 
     @Test
-    public void diffTwoPositiveResPos() {assertEquals(calculator.diff(1,6), -5);}
+    @DisplayName("Верная разность двух положительных")
+    void diffTwoPositiveResPos() {
+        assertEquals(calculator.diff(1, 6), -5);
+    }
 
     @Test
-    public void diffTwoPositiveResNeg() {assertNotEquals(calculator.diff(1,6), 5);}
+    @DisplayName("Неверная разность двух положительных")
+    void diffTwoPositiveResNeg() {
+        assertNotEquals(calculator.diff(1, 6), 5);
+    }
 
     @Test
-    public void diffTwoNegativeResPos() {assertEquals(calculator.diff(-10, -5), -5);}
+    @DisplayName("Верная разность двух отрицательных")
+    void diffTwoNegativeResPos() {
+        assertEquals(calculator.diff(-10, -5), -5);
+    }
 
     @Test
-    public void diffTwoNegativeResNeg() {assertNotEquals(calculator.add(-10, -5), -5);}
+    @DisplayName("Неверная разность двух отрицательных")
+    void diffTwoNegativeResNeg() {
+        assertNotEquals(calculator.add(-10, -5), -5);
+    }
 
     @Test
-    public void diffOppositeResPos() {assertEquals(calculator.diff(-5, 11), -16);}
+    @DisplayName("Верная разность чисел с разным знаком")
+    void diffOppositeResPos() {
+        assertEquals(calculator.diff(-5, 11), -16);
+    }
 
     @Test
-    public void diffOppositeResNeg() {assertNotEquals(calculator.add(5, -11), -16);}
+    @DisplayName("Неверная разность чисел с разным знаком")
+    void diffOppositeResNeg() {
+        assertNotEquals(calculator.add(5, -11), -16);
+    }
 
-
-//    @Test
-//    public void sum
 }
